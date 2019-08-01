cee62e2ebfd764bedd383559bad795270b321854
cjustinobi <cjustinobi@gmail.com>
2019-08-01 19:46:14 +0100

    blog

---
+++ pages/blog.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <h1>Blog content</h1>
+    </div>
+</template>
+
+<script>
+
+    export default {
+        data() {
+            return {
+                details: ''
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
